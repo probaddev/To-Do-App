8f0ce83c82c4fc782f9317ca40124db53e716bae
KVG <probaddev@gmail.com>
2020-08-29 15:26:28 +0300

    Установил и настроил webpack

---
+++ dist/webpack.config.dev.js
@@ -0,0 +1,13 @@
+"use strict";
+
+var path = require("path");
+
+module.exports = {
+  mode: "development",
+  entry: "./src/main.js",
+  output: {
+    filename: "bundle.js",
+    path: path.join(__dirname, "public")
+  },
+  devtool: "source-map"
+};
