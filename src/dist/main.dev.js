51d0e7b0b1e418acc9b588d33cb4c771dfa701f6
KVG <probaddev@gmail.com>
2020-09-01 11:10:22 +0300

    Импортировал компоненты в точку входа. Создал файл для настройки Prettier

--- src/dist/main.dev.js
+++ src/dist/main.dev.js
@@ -1,5 +1,19 @@
 "use strict";
 
+var _board = require("./components/board.js");
+
+var _siteMenu = require("./components/site-menu");
+
+var _filter = require("./components/filter.js");
+
+var _sorting = require("./components/sorting.js");
+
+var _taskEdit = require("./components/task-edit.js");
+
+var _task = require("./components/task.js");
+
+var _loadMoreButton = require("./components/load-more-button.js");
+
 var TASK_COUNT = 3;
 
 var render = function render(container, template, place) {
@@ -8,16 +22,16 @@ var render = function render(container, template, place) {
 
 var siteMainElement = document.querySelector(".main");
 var siteHeaderElement = siteMainElement.querySelector(".main__control");
-render(siteHeaderElement, createSiteMenuTemplate(), "beforeend");
-render(siteMainElement, createFilterTemplate(), "beforeend");
-render(siteMainElement, createBoardTemplate(), "beforeend");
+render(siteHeaderElement, (0, _siteMenu.createSiteMenuTemplate)(), "beforeend");
+render(siteMainElement, (0, _filter.createFilterTemplate)(), "beforeend");
+render(siteMainElement, (0, _board.createBoardTemplate)(), "beforeend");
 var boardElement = siteMainElement.querySelector(".board");
 var taskListElement = siteMainElement.querySelector(".board__tasks");
-render(boardElement, createSortingTemplate(), "afterbegin");
-render(taskListElement, createTaskEditTemplate(), "beforeend");
+render(boardElement, (0, _sorting.createSortingTemplate)(), "afterbegin");
+render(taskListElement, (0, _taskEdit.createTaskEditTemplate)(), "beforeend");
 
 for (var i = 0; i < TASK_COUNT; i++) {
-  render(taskListElement, createTaskTemplate(), "beforeend");
+  render(taskListElement, (0, _task.createTaskTemplate)(), "beforeend");
 }
 
-render(boardElement, createLoadMoreButtonTemplate(), "beforeend");
+render(boardElement, (0, _loadMoreButton.createLoadMoreButtonTemplate)(), "beforeend");
--- src/main.js
+++ src/main.js
@@ -1,3 +1,11 @@
+import {createBoardTemplate} from "./components/board.js";
+import {createSiteMenuTemplate} from "./components/site-menu";
+import {createFilterTemplate} from "./components/filter.js";
+import {createSortingTemplate} from "./components/sorting.js";
+import {createTaskEditTemplate} from "./components/task-edit.js";
+import {createTaskTemplate} from "./components/task.js";
+import {createLoadMoreButtonTemplate} from "./components/load-more-button.js";
+
 const TASK_COUNT = 3;
 
 const render = (container, template, place) => {
