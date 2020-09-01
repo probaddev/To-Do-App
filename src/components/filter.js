dfb835dc3a37553e6f55e60f9278155edee2989f
KVG <probaddev@gmail.com>
2020-09-01 11:39:49 +0300

    Вынес фильтр ALL в отдельную функцию

--- src/components/filter.js
+++ src/components/filter.js
@@ -1,6 +1,5 @@
-export const createFilterTemplate = () => {
-  return `<section class="main__filter filter container">
-      <input
+const createFilterMarkup = () => {
+  return `<input
         type="radio"
         id="filter__all"
         class="filter__input visually-hidden"
@@ -9,7 +8,13 @@ export const createFilterTemplate = () => {
       />
       <label for="filter__all" class="filter__label">
         All <span class="filter__all-count">13</span></label
-      >
+      >`;
+};
+
+export const createFilterTemplate = () => {
+  const filterMarkup = createFilterMarkup();
+  return `<section class="main__filter filter container">
+      ${filterMarkup}
       <input
         type="radio"
         id="filter__overdue"
