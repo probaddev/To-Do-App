a1dd4f1a469442f42062d8e5146052ff35d26487
KVG <probaddev@gmail.com>
2020-09-12 09:20:46 +0300

    Перенёс массив данных в mock/filter.js

---
+++ src/mock/filter.js
@@ -0,0 +1,30 @@
+const generateFilters = () => {
+  return [
+    {
+      name: "all",
+      count: 18,
+    },
+    {
+      name: "overdue",
+      count: 18,
+    },
+    {
+      name: "today",
+      count: 18,
+    },
+    {
+      name: "favourites",
+      count: 18,
+    },
+    {
+      name: "repeating",
+      count: 18,
+    },
+    {
+      name: "archive",
+      count: 18,
+    },
+  ];
+};
+
+export {generateFilters};
