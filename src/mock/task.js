ce08db765fef53cd793054dc5e5c5bcd8048e44c
KVG <probaddev@gmail.com>
2020-09-21 00:28:17 +0300

    feat: add logic of generation markup based on repeatingDays

--- src/mock/task.js
+++ src/mock/task.js
@@ -1,9 +1,19 @@
+const DefaultRepeatingDays = {
+  mo: false,
+  tu: false,
+  we: false,
+  th: false,
+  fr: false,
+  sa: false,
+  su: false,
+};
+
 const generateTask = () => {
   return {
     description: "Example default task with default color.",
     dueDate: Math.random() > 0.5 ? new Date() : null,
     color: "pink",
-    repeatingDays: null,
+    repeatingDays: Object.assign({}, DefaultRepeatingDays, {mo: Math.random() > 0.5}),
     isArchive: Math.random() > 0.5,
     isFavorite: Math.random() > 0.5,
   };
